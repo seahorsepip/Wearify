5b10bc6f3fbabbbbc2b7036f8bed405d71c49f1c
seahorsepip <seahorsepip@outlook.com>
2018-02-01 12:30:45 +0100

    Toggle ambient support based on drawer state (open or closed/peek).

--- app/src/main/java/com/seapip/thomas/wearify/LibraryAltActivity.kt
+++ app/src/main/java/com/seapip/thomas/wearify/LibraryAltActivity.kt
@@ -6,9 +6,7 @@ import android.graphics.Color
 import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
 import android.support.wear.ambient.AmbientMode
-import android.support.wear.ambient.AmbientMode.FRAGMENT_TAG
 import android.support.wear.widget.drawer.WearableDrawerLayout
-import android.support.wear.widget.drawer.WearableDrawerView
 import android.view.View
 import com.seapip.thomas.wearify.R.color.primary_icon
 import com.seapip.thomas.wearify.R.drawable.*
@@ -17,47 +15,65 @@ import kotlinx.android.synthetic.main.activity_library.*
 
 class LibraryAltActivity : Activity(), AmbientMode.AmbientCallbackProvider {
 
-    private var mAmbientController: AmbientMode.AmbientController? = null
+    private var mAmbientMode: AmbientMode? = null
+    private var mAmbientAttached = false
+    private val mDrawerStateCallback = object : WearableDrawerLayout.DrawerStateCallback() {
+        override fun onDrawerStateChanged(layout: WearableDrawerLayout?, newState: Int) {
+            super.onDrawerStateChanged(layout, newState)
+            if (newState == 0) {
+                if ((playback_drawer.isClosed || playback_drawer.isPeeking)) removeAmbientSupport()
+                else if (playback_drawer.isOpened) addAmbientSupport()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_library)
 
-        drawer_layout.setDrawerStateCallback(object : WearableDrawerLayout.DrawerStateCallback() {
-            override fun onDrawerClosed(layout: WearableDrawerLayout?, drawerView: WearableDrawerView?) {
-                super.onDrawerClosed(layout, drawerView)
-                //Not being called on close ugh.....
-            }
-
-            override fun onDrawerOpened(layout: WearableDrawerLayout?, drawerView: WearableDrawerView?) {
-                super.onDrawerOpened(layout, drawerView)
-                if (drawerView == now_playing_drawer && mAmbientController == null) {
-                    mAmbientController = AmbientMode.attachAmbientSupport(this@LibraryAltActivity)
-                }
-            }
-        })
+        drawer_layout.setDrawerStateCallback(mDrawerStateCallback)
 
         val drawablePlaying = getDrawable(ic_logo_waves_animated) as AnimatedVectorDrawable
         icon_drawer_peek.setImageDrawable(drawablePlaying)
         drawablePlaying.start()
 
-        button_play.setOnClickListener {
-            if (mAmbientController != null) {
-                val ambientFragment = fragmentManager.findFragmentByTag(FRAGMENT_TAG)
+        playback_drawer.controller.peekDrawer()
+    }
+
+    private fun addAmbientSupport() {
+        if (!mAmbientAttached) {
+            mAmbientAttached = true
+
+            if (mAmbientMode == null) AmbientMode.attachAmbientSupport(this@LibraryAltActivity)
+            else fragmentManager
+                    .beginTransaction()
+                    .add(mAmbientMode, AmbientMode.FRAGMENT_TAG)
+                    .commit()
+        }
+    }
+
+    private fun removeAmbientSupport() {
+        if (mAmbientAttached) {
+            mAmbientAttached = false
+
+            if (mAmbientMode == null) {
+                mAmbientMode = fragmentManager.findFragmentByTag(AmbientMode.FRAGMENT_TAG) as AmbientMode?
+            }
+            if (mAmbientMode != null) {
                 fragmentManager
                         .beginTransaction()
-                        .remove(ambientFragment)
+                        .remove(mAmbientMode)
                         .commit()
+
             }
         }
-
-        now_playing_drawer.controller.peekDrawer()
     }
 
     override fun getAmbientCallback(): AmbientMode.AmbientCallback {
         return object : AmbientMode.AmbientCallback() {
             override fun onExitAmbient() {
                 super.onExitAmbient()
+
                 //Background
                 album_art_background_image.visibility = View.VISIBLE
 
@@ -74,21 +90,21 @@ class LibraryAltActivity : Activity(), AmbientMode.AmbientCallbackProvider {
 
             override fun onEnterAmbient(ambientDetails: Bundle?) {
                 super.onEnterAmbient(ambientDetails)
+                val burnIn = ambientDetails!!.getBoolean(AmbientMode.EXTRA_BURN_IN_PROTECTION)
+                val lowBit = ambientDetails.getBoolean(AmbientMode.EXTRA_LOWBIT_AMBIENT)
+
                 //Background
-                album_art_background_image.visibility = View.INVISIBLE
+                if (burnIn || lowBit) album_art_background_image.visibility = View.INVISIBLE
 
                 //Progressbar
                 progress_bar.visibility = View.INVISIBLE
 
                 //Play button
                 button_play.apply {
-                    background = getDrawable(round_primary_ambient_button)
-                    setImageDrawable(getDrawable(ic_pause_black_burn_in_24dp))
-                    imageTintList = ColorStateList.valueOf(Color.WHITE)
+                    if (burnIn || lowBit) background = getDrawable(round_primary_ambient_button)
+                    if (burnIn) setImageDrawable(getDrawable(ic_pause_black_burn_in_24dp))
+                    if (burnIn || lowBit) imageTintList = ColorStateList.valueOf(Color.WHITE)
                 }
-
-                //Show now playing view in ambient mode
-                //if (mAmbientController != null) now_playing_drawer.controller.openDrawer()
             }
         }
     }
